d131c7e0e480be3fc71c7df96593368655bc4345
Scott Taylor <scott.c.taylor@mac.com>
2017-08-15 11:23:14 -0400

    clean up configuration

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { IntlProvider, addLocaleData } from 'react-intl';
 import { graphql } from 'react-apollo';
 import { routerShape } from 'found/lib/PropTypes';
 import {
@@ -10,23 +9,13 @@ import {
   Primary,
   Secondary,
 } from 'wp-styled-components/lib/App';
-import theme from 'wp-styled-components/lib/theme';
 import 'wp-styled-components/lib/global';
+import IntlProvider from 'decorators/IntlProvider';
 import Settings from 'components/Settings';
 import Header from 'components/Header';
 import Sidebar from 'components/Sidebar';
 import Footer from 'components/Footer';
 import AppQuery from 'graphql/App_Query.graphql';
-
-const langCache = {};
-const getMessages = locale => {
-  if (langCache[locale]) {
-    return langCache[locale];
-  }
-  // eslint-disable-next-line global-require, import/no-dynamic-require
-  langCache[locale] = require(`../langs/${locale}.js`).default;
-  return langCache[locale];
-};
 
 @graphql(AppQuery, {
   options: {
@@ -36,6 +25,7 @@ const getMessages = locale => {
     },
   },
 })
+@IntlProvider
 export default class App extends Component {
   static propTypes = {
     data: PropTypes.shape({
@@ -57,20 +47,10 @@ export default class App extends Component {
     router: routerShape.isRequired,
   };
 
-  state = {
-    locale: 'en',
-  };
-
   constructor(props, context) {
     super(props, context);
 
     this.removeTransitionHook = props.router.addTransitionHook(this.onTransition);
-  }
-
-  componentDidMount() {
-    // eslint-disable-next-line global-require, import/no-dynamic-require
-    const localeData = require(`react-intl/locale-data/${this.state.locale}`);
-    addLocaleData(localeData);
   }
 
   componentWillUnmount() {
@@ -94,25 +74,20 @@ export default class App extends Component {
     const { viewer: { settings, navMenu, sidebar } } = data;
 
     return (
-      <ThemeProvider theme={theme}>
-        <IntlProvider
-          locale={this.state.locale}
-          messages={getMessages(this.state.locale)}
-        >
-          <PageWrapper>
-            <Settings settings={settings} locale={this.state.locale} />
-            <Header {...{ settings, navMenu }} />
-            <Content>
-              <Primary>
-                {children}
-              </Primary>
-              <Secondary>
-                <Sidebar sidebar={sidebar} />
-              </Secondary>
-            </Content>
-            <Footer />
-          </PageWrapper>
-        </IntlProvider>
+      <ThemeProvider>
+        <PageWrapper>
+          <Settings settings={settings} />
+          <Header {...{ settings, navMenu }} />
+          <Content>
+            <Primary>
+              {children}
+            </Primary>
+            <Secondary>
+              <Sidebar sidebar={sidebar} />
+            </Secondary>
+          </Content>
+          <Footer />
+        </PageWrapper>
       </ThemeProvider>
     );
   }
